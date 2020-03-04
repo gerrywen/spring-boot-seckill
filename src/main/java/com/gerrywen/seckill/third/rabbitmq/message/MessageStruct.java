40f0e7a624e46d87cca0942d8e10874139214668
xinyar <guoli.wen@xinyartech.com>
2020-03-04 10:45:36 +0800

    增加rabbitmq服务

---
+++ src/main/java/com/gerrywen/seckill/third/rabbitmq/message/MessageStruct.java
@@ -0,0 +1,24 @@
+package com.gerrywen.seckill.third.rabbitmq.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * description:
+ *
+ * @author wenguoli
+ * @date 2020/3/4 9:58
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageStruct implements Serializable {
+    private static final long serialVersionUID = 392365881428311040L;
+
+    private String message;
+}
