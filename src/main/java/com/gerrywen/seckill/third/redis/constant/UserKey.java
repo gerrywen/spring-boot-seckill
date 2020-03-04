0b10d39ef8db8902245e492577e3985b96f4e39d
xinyar <guoli.wen@xinyartech.com>
2020-03-04 08:44:37 +0800

    用户rediskey

---
+++ src/main/java/com/gerrywen/seckill/third/redis/constant/UserKey.java
@@ -0,0 +1,22 @@
+package com.gerrywen.seckill.third.redis.constant;
+
+/**
+ * description: 用户主键
+ *
+ * @author wenguoli
+ * @date 2020/3/4 8:40
+ */
+public class UserKey {
+
+    /**
+     * 用户ID
+     */
+    public static final String USER_ID_KEY_PREFIX = "seckill:user:id";
+
+    /**
+     * 用户名称
+     */
+    public static final String USER_NAME_KEY_PREFIX = "seckill:user:name";
+
+
+}
